12096e546b76ae209d118b21347267cb903ee1ef
hirsang whisnu <hirsangwhisnu@gmail.com>
2024-07-23 22:48:51 +0700

    menambahkan file ekyc dan updated run config

--- RunConfig.js
+++ RunConfig.js
@@ -1,25 +1,25 @@
-const username ='mandiridk@mail7.io'
-const password ='Swadaya2'
-const borrowerdashboard='https://borrower.dompetkilat.dev'
-const passwordPertama='ttastats'
+export const username ='kidawot653@tiervio.com'
+export const password ='Swadaya2'
+export const borrowerdashboard='https://borrower.dompetkilat.dev'
+export const passwordPertama='TLb7f6773d36'
 
-function generateIdCardNumber() {
-    const prefix = '3276';
+export function generateIdCardNumber() {
+    const prefix = '3999';
     const timestamp = Date.now().toString().slice(-12); // Mengambil 12 digit terakhir dari timestamp
     const randomDigits = Math.floor(Math.random() * 10000).toString().padStart(5, '0'); // Empat digit acak
     return (prefix + timestamp + randomDigits).slice(0, 16); // Menggabungkan dan memastikan panjang tidak melebihi 16 digit
 }
-function generatePhoneNumber() {
-    const prefix = '8';
+export function generatePhoneNumber() {
+    const prefix = '080';
     const randomDigits = Math.floor(Math.random() * 10000000000).toString().padStart(10, '0'); // Sembilan digit acak
     return (prefix + randomDigits).slice(0, 12); // Menggabungkan dan memastikan panjang tidak melebihi 12 digit
 }
-function generateRekeningNumber() {
+export function generateRekeningNumber() {
     const prefix = '12';
     const randomDigits = Math.floor(Math.random() * 10000000000).toString().padStart(10, '0'); // Sembilan digit acak
     return (prefix + randomDigits).slice(0, 10); 
 }
-function generateEmail(length) {
+export function generateEmail(length) {
     const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
     let randomText = '';
     for (let i = 0; i < length; i++) {
@@ -27,12 +27,41 @@ function generateEmail(length) {
     }
     return randomText;
 }
-function generateRandomName(prefix) {
-    const names = ["Dompet", "Kilat", "Mudah", "Aman", "Cerdas"];
+export function generatenama() {
+    const firstNames = [
+        "Adi", "Budi", "Cahyo", "Dewi", "Eko", "Fajar", "Gita", "Hadi", "Ika", "Joko",
+        "Kartika", "Lina", "Mira", "Nia", "Oka", "Putri", "Rizki", "Sari", "Teguh", "Umi"
+    ];
+    const lastNames = [
+        "Santoso", "Wijaya", "Saputra", "Pratama", "Suharto", "Gunawan", "Wibowo", "Purnomo", "Rahayu", "Lestari",
+        "Anggraini", "Susilo", "Rahardjo", "Yulianto", "Utami", "Hermanto", "Sudarmo", "Nugroho", "Handayani", "Setiawan"
+    ];
+    const randomFirstName = firstNames[Math.floor(Math.random() * firstNames.length)];
+    const randomLastName = lastNames[Math.floor(Math.random() * lastNames.length)];
+
+    return `${randomFirstName} ${randomLastName}`;
+}
+export function generateRandomNumber(length) {
+    const characters = '1234567890';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+}
+export const getNama = () => {
+    const nama = Cypress.env('nama', generatenama());
+    if (!nama) {
+    throw new Error('nama tidak tersedia');
+    }
+    return nama;
+};
+export function generateRandomName(prefix) {
+    const names = ["Dompet", "Kilat", "Mudah", "Aman", "Cerdas", "Jaya"];
     const randomName = names[Math.floor(Math.random() * names.length)];
     return prefix + randomName ;
 }
-const login = () => {
+export const login = () => {
     cy.visit(borrowerdashboard);
     cy.xpath("//h2[normalize-space()='KEBIJAKAN DAN PRIVACY']", { timeout: 10000 }).should('be.visible');
     cy.get('#mui-1').click();
@@ -44,15 +73,11 @@ const login = () => {
     cy.get('[data-test-qa="verify-ekyc"]').should('be.visible');
     cy.get('[data-test-qa="verify-ekyc"]').click();
 }
+// data foto
+export const MaxSize = 'MaxFile.jpg'
+export const FilePDF ='FilePDF.pdf'
+export const Data1 = 'Data1.jpg'
+export const Data2 = 'Data2.png'
+export const Data3 = 'Data3.png'
 
-module.exports = { 
-    login,
-    username, 
-    password,
-    passwordPertama, 
-    borrowerdashboard, 
-    generateIdCardNumber, 
-    generatePhoneNumber, 
-    generateEmail,
-    generateRandomName
-};
+
---
+++ cypress/e2e/Ekyc-Corporate.cy.js
@@ -0,0 +1,134 @@
+
+const { login, username, password, borrowerdashboard, generateIdCardNumber, generatePhoneNumber, generateRandomName, generateEmail, generateIdNPWPNumber, generateSIUPNumber, Data1, Data2, Data3, MaxSize, FilePDF, nama, name, generateRekeningNumber, generatenama, getNama, generateRandomNumber } = require("../../RunConfig");
+import 'cypress-file-upload';
+
+describe('EKYC-Borrower-Personal', ()=> {
+    it('Step Data Diri', ()=> {
+        login(username, password, borrowerdashboard);
+        cy.get('#email',{timeout:5000}).should('have.value', username)
+        cy.wait(1000).get('#name').type(getNama());
+        cy.wait(300).get('#phoneNumber').type(generatePhoneNumber());
+        cy.wait(300).get('#idCardNumber').type(generateIdCardNumber())
+        cy.wait(300).get('#familyCardNumber').type(generateIdCardNumber())
+        cy.wait(300).get('#birthPlace').type('KANDANG AYAM')
+        cy.wait(300).xpath('//button[@aria-label="Choose date"]').click()
+        cy.wait(300).xpath("//button[@aria-label='calendar view is open, switch to year view']//*[name()='svg']").click()
+        cy.wait(300).xpath("//button[normalize-space()='2000']").click()
+        cy.wait(300).xpath("//button[normalize-space()='8']").click()
+        cy.wait(300).get('#genderId').click()
+        cy.wait(300).get('#genderId-option-0').click()
+        cy.wait(300).get('#numberOfFamilyMembers').type('11')
+        cy.wait(300).get('#motherMaidenName').type('Mamah DeDeH')
+        cy.wait(300).get('#homeOwnershipStatusId').click()
+        cy.wait(300).get('#homeOwnershipStatusId-option-1').click()
+        cy.wait(300).get('[data-test-qa="open-popup"]').click()
+        cy.wait(300).xpath("//p[@id='alert-dialog-description']").should('be.visible');
+        cy.wait(300).get('[data-test-qa="next"]').click()
+        cy.wait(1000).xpath("//button[@aria-label='menu']").click()
+        cy.wait(300).get('[data-test-qa="logout"]').click()
+    })
+    it('Step Data Alamat', ()=> {
+        login(username, password, borrowerdashboard);
+        cy.wait(500).get('#address').type('Jl In Aja Dulu')
+        cy.wait(500).get('#neighborhood').type('5')
+        cy.wait(500).get('#hamlet').type('6')
+        cy.wait(500).get('#provinceId').click()       
+        cy.wait(500).xpath('//li[normalize-space()="JAWA BARAT"]').click()
+        cy.wait(500).get('#districtId').click()
+        cy.wait(500).xpath('//li[normalize-space()="DEPOK"]').click()
+        cy.wait(500).get('#subDistrictId').click()
+        cy.wait(500).xpath('//li[normalize-space()="TAPOS"]').click()
+        cy.wait(500).get('#urbanVillageId').click()
+        cy.wait(500).xpath('//li[normalize-space()="JATIJAJAR"]').click()
+        cy.get('#postalCode',{timeout:5000}).should('have.value','16451')
+        cy.wait(300).get('[data-test-qa="next"]').click()
+        cy.wait(1000).xpath("//button[@aria-label='menu']").click()
+        cy.wait(500).get('[data-test-qa="logout"]').click()
+    })
+    it('Step Data Perusahaan', ()=> {
+        login(username, password, borrowerdashboard);
+        cy.wait(2000).get('#name').type(generateRandomName('PT '))
+        cy.wait(1500).get('#phoneNumber').type(generatePhoneNumber())
+        cy.wait(300).get('#directorName').type(getNama())
+        cy.wait(300).get('#directorship').type('Bos Kacunk')
+        cy.wait(300).get('#directorIdCardNumber').type(generateIdCardNumber())
+        cy.wait(500).get('#email').type(generateEmail(8)+'@mail7.io')
+        cy.wait(300).get('#buildingOwnershipStatusId').click()
+        cy.wait(300).get('#buildingOwnershipStatusId-option-0').click()
+        cy.wait(300).get('#establishmentDeedNumber').type('213')
+        cy.wait(300).get('[data-testid="CalendarIcon"]').click()
+        cy.wait(300).xpath('//button[@aria-label="calendar view is open, switch to year view"]//*[name()="svg"]').click()
+        cy.wait(300).xpath("//button[normalize-space()='2010']").click()
+        cy.wait(300).xpath("//button[normalize-space()='10']").click()
+        cy.wait(300).get('#establishmentLocation').type('TANAH')
+        cy.wait(300).get('#legalEntityId').click()
+        cy.wait(300).get('#legalEntityId-option-0').click()
+        cy.wait(300).get('#businessTradePermitNumber').type(generateRandomNumber(15))
+        cy.wait(300).get('#lastAmendmentDeedNumber').type('327')
+        cy.wait(300).get('#tinCardNumber').type(generateRandomNumber(15))
+        cy.wait(300).get('#industrialSectorId').click()
+        cy.wait(300).get('#industrialSectorId-option-8').click()
+        cy.wait(300).get('#onlineJobId').click()
+        cy.wait(300).get('#onlineJobId-option-0').click()
+        cy.wait(300).get('#monthlyIncome').type('12000000')
+        cy.wait(300).get('#asset').type('12000000000')
+        cy.wait(300).get('#address').type('Jl Doang')
+        cy.wait(500).get('#provinceId').click()       
+        cy.wait(500).xpath('//li[normalize-space()="JAWA BARAT"]').click()
+        cy.wait(500).get('#districtId').click()
+        cy.wait(500).xpath('//li[normalize-space()="DEPOK"]').click()
+        cy.wait(500).get('#subDistrictId').click()
+        cy.wait(500).xpath('//li[normalize-space()="TAPOS"]').click()
+        cy.wait(500).get('#urbanVillageId').click()
+        cy.wait(500).xpath('//li[normalize-space()="CILANGKAP"]').click()
+        cy.get('#postalCode',{timeout:5000}).should('have.value','16458')
+        cy.wait(500).get('[data-test-qa="next"]').click()
+        cy.wait(1000).xpath("//button[@aria-label='menu']").click()
+        cy.wait(300).get('[data-test-qa="logout"]').click()
+    })
+    it('Step Data Upload Doc PIC', ()=> {
+        login(username, password, borrowerdashboard);
+        //Test Foto Selfie
+        cy.wait(1000).get('#selfie').attachFile(MaxSize)
+        cy.wait(1000).xpath('//*[contains(text(),"Foto Selfie harus kurang dari 900KB")]').should('be.visible')
+        cy.wait(1000).get('#selfie').attachFile(FilePDF)
+        cy.wait(1000).xpath('//*[contains(text(),"Foto Selfie tidak valid")]').should('be.visible')
+        cy.wait(1000).get('#selfie').attachFile(Data1)
+        //Test Foto KTP
+        cy.wait(1000).get('#identityCard').attachFile(MaxSize)
+        cy.wait(1000).xpath('//*[contains(text(),"Foto KTP harus kurang dari 900KB")]').should('be.visible')
+        cy.wait(1000).get('#identityCard').attachFile(FilePDF)
+        cy.wait(1000).xpath('//*[contains(text(),"Foto KTP tidak valid")]').should('be.visible')
+        cy.wait(1000).get('#identityCard').attachFile(Data2)
+        //Test Foto NPWP
+        cy.wait(1000).get('#tinCard').attachFile(MaxSize)
+        cy.wait(1000).xpath('//*[contains(text(),"Foto NPWP harus kurang dari 900KB")]').should('be.visible')
+        cy.wait(1000).get('#tinCard').attachFile(FilePDF)
+        cy.wait(1000).xpath('//*[contains(text(),"Foto NPWP tidak valid")]').should('be.visible')
+        cy.wait(1000).get('#tinCard').attachFile(Data3)
+
+        cy.wait(300).get('[data-test-qa="open-poup"]').click()
+        cy.wait(300).xpath("//p[@id='alert-dialog-description']").should('be.visible');
+        cy.wait(300).get('[data-test-qa="next"]').click()
+        cy.wait(3000).xpath("//button[@aria-label='menu']").click()
+        cy.wait(300).get('[data-test-qa="logout"]').click()
+    })
+    
+    it('Step Data Bank', ()=>{
+        login(username, password, borrowerdashboard);
+        cy.wait(1500).get('#accountHolderName').type(getNama())
+        cy.wait(500).get('#bankId').click()
+        cy.wait(500).get('#bankId-option-19').click()
+        cy.wait(1000).get('#accountNumber').type(generateRekeningNumber())
+        cy.wait(1000).xpath('//*[contains(text(),"Serta menyetujui dan mengizinkan penggunaan, pemrosesan, dan transfer Data Pribadi saya sebagaimana dijelaskan di dalamnya, termasuk mengizinkan penyedia layanan PT Indo Fin Tek (Dompet Kilat) untuk melakukan penggunaan, pemrosesan, dan transfer Data Pribadi sesuai kebijakan Perusahaan yang berlaku. Dapat diakses pada laman berikut")]').should('be.visible')
+        // cy.wait(300).xpath("//span[normalize-space()='Kebijakan Privasi Pengguna.']").click()
+        cy.wait(300).get('[aria-label="ant design"]').click()
+        cy.wait(500).xpath("//h2[normalize-space()='KEBIJAKAN PRIVASI PENGGUNA']")
+        cy.wait(300).get('[data-test-qa="close-popup-snk"]').click()
+        cy.wait(300).get('[data-test-qa="submit"]').click()
+        cy.wait(1500).xpath('//*[contains(text(),"Harap Menunggu Hasil Verifikasi Data..")]')
+    })
+        //     //[Mandiri-18][bca-19][BRI-12][BNI-24][SAHABAT SAMPOERNA-22][PERMATA-17]
+    
+
+})
